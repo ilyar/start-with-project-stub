4126ca802321b88773fffc95d2776d6015222985
Ilya Rogov <ilyar.software@gmail.com>
2017-01-03 21:17:55 +0200

    BEMHTML templates (create block goods)

---
+++ desktop.blocks/goods/goods.bemhtml.js
@@ -0,0 +1,56 @@
+block('goods')(
+    tag()('ul'),
+
+    content()(function () {
+        return this.ctx.goods.map(function (item) {
+            return [
+                {
+                    elem : 'item',
+                    elemMods : { new : item.new && 'yes' },
+                    content : [
+                        {
+                            elem : 'title',
+                            content : item.title
+                        },
+                        {
+                            elem : 'image',
+                            url : item.image
+                        },
+                        {
+                            elem : 'price',
+                            content : {
+                                block : 'link',
+                                mix : [{ block : 'goods', elem : 'link' }],
+                                url : item.url,
+                                content : item.price
+                            }
+                        }
+                    ]
+                },
+                ' '
+            ];
+
+        });
+
+    }),
+
+    elem('item')(
+        tag()('li')
+    ),
+
+    elem('title')(
+        tag()('h3')
+    ),
+
+    elem('image')(
+        tag()('img'),
+
+        attrs()(function () {
+            return { src : this.ctx.url };
+        })
+    ),
+
+    elem('price')(
+        tag()('span')
+    )
+);
